de89ff4a493fc8799bf2beafa4dce5281770d339
Nicolas Perriault <nperriault@gmail.com>
2015-02-23 09:36:13 +0100

    Allowing port to be configured through env.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/get", function(req, res) {
   });
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Server listening at http://%s:%s", host, port);
